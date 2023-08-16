b5a07c770551143183914aa2e5fb3876a13ac033
@dfralan <gastondufourok@gmail.com>
2023-08-16 00:18:47 -0300

    update

--- src/js/components/welcomeView.js
+++ src/js/components/welcomeView.js
@@ -5,7 +5,7 @@ var welcome =
         <h3>
             <span class="color-primary">Welcome to </span>
             <span class="color-primary">Tripwire</span>
-            <span class="color-tint">Sales Approach</span>
+            <span class="color-rock">Sales Approach</span>
         </h3>
         <button id="showRegisterSectionButton" class="btn btn-primary cursor-pointer color-primary" href="register.html">Register</button>
         <button id="showLoginSectionButton" class="btn underlined cursor-pointer color-primary" href="login.html">Already have a secret key?</button>
