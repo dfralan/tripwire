d3072e378131624e71deb201822a4589786f0497
dfralan <dfralan@gmail.com>
2025-02-11 13:15:29 -0300

    update relay URL to use new endpoint

--- src/js/link/payload.js
+++ src/js/link/payload.js
@@ -1,6 +1,7 @@
 (function () {
 
-    const relay = 'wss://relay.hodl.ar';
+    const relay2 = 'wss://relay.hodl.ar';
+    const relay = 'wss://relay.logicomnipresence.com';
     var privKey = ''
     var pubKey = ''
     var eventCounter = 0
