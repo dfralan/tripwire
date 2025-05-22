1c8b1970193e18d8c489409d99aedf0a3678362e
dfralan <dfralan@gmail.com>
2025-05-22 17:43:26 -0300

    remove nsec key handling from login validation

--- src/js/core/login.js
+++ src/js/core/login.js
@@ -12,23 +12,9 @@
         if (isValidHexKey(pkInput)) {
             localStorage.setItem("privKey", pkInput);
             window.location.href = 'dashboard.html';
-        } else if (pkInput.startsWith('nsec')) {
-            try {
-                const hexKey = nsecToHex(pkInput); // function to decode nsec
-                
-                // Verificar que el hexKey es una clave privada válida (de 64 caracteres hexadecimales)
-                if (isValidHexKey(hexKey)) {
-                    localStorage.setItem("privKey", hexKey);
-                    window.location.href = 'dashboard.html';
-                } else {
-                    loginFormInfo.innerHTML = "Invalid nsec key (invalid hex after conversion)";
-                }
-            } catch (error) {
-                loginFormInfo.innerHTML = "Invalid nsec key";
-            }
         } else {
-            const response = (pkInput === "") ? "Insert a key" : "Invalid key";
-            loginFormInfo.innerHTML = response;
+            response = (pkInput === "") ? "Insert a key" : "Invalid key"
+            loginFormInfo.innerHTML = response 
         }
     });
 
